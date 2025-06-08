5d99eea305b48ad979667ed6a4b4ccbfaed0f81c
Bertan Günyel <bertan.gunyel@gmail.com>
2025-06-08 20:02:29 +0300

    Max results per query is added to web search

--- src/ai_common/web_search.py
+++ src/ai_common/web_search.py
@@ -12,12 +12,14 @@ class WebSearch:
                  search_category: TavilySearchCategory,
                  number_of_days_back: int,
                  max_tokens_per_source: int = 5000,
+                 max_results_per_query: int = 5,
                  include_raw_content: bool = False) -> None:
         self.event_loop = asyncio.get_event_loop()
         self.api_key = api_key
         self.search_category = search_category
         self.number_of_days_back = number_of_days_back
         self.max_tokens_per_source = max_tokens_per_source
+        self.max_results_per_query = max_results_per_query
         self.include_raw_content = include_raw_content
 
     def search(self, search_queries: list[str]) -> dict:
@@ -27,7 +29,8 @@ class WebSearch:
                 client=AsyncTavilyClient(api_key=self.api_key),
                 search_queries=search_queries,
                 search_category=self.search_category,
-                number_of_days_back=self.number_of_days_back
+                number_of_days_back=self.number_of_days_back,
+                max_results=self.max_results_per_query,
             )
         )
 

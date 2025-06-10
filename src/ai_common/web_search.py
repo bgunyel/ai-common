ca5d9b172940b3f702df82cc2d335b1567e19503
Bertan Günyel <bertan.gunyel@gmail.com>
2025-06-10 10:55:20 +0300

    WebSearch modified

--- src/ai_common/web_search.py
+++ src/ai_common/web_search.py
@@ -7,38 +7,24 @@ from .utils import tavily_search_async, deduplicate_sources
 
 
 class WebSearch:
-    def __init__(self,
-                 api_key: str,
-                 search_category: TavilySearchCategory,
-                 number_of_days_back: int,
-                 max_tokens_per_source: int = 5000,
-                 max_results_per_query: int = 5,
-                 include_raw_content: bool = False) -> None:
+    def __init__(self, api_key: str) -> None:
         self.event_loop = asyncio.get_event_loop()
         self.api_key = api_key
-        self.search_category = search_category
-        self.number_of_days_back = number_of_days_back
-        self.max_tokens_per_source = max_tokens_per_source
-        self.max_results_per_query = max_results_per_query
-        self.include_raw_content = include_raw_content
 
-    def search(self, search_queries: list[str]) -> dict:
+    def search(self,
+               search_queries: list[str],
+               search_category: TavilySearchCategory,
+               number_of_days_back: int,
+               max_results_per_query: int) -> dict:
 
         search_docs = self.event_loop.run_until_complete(
             tavily_search_async(
                 client=AsyncTavilyClient(api_key=self.api_key),
                 search_queries=search_queries,
-                search_category=self.search_category,
-                number_of_days_back=self.number_of_days_back,
-                max_results=self.max_results_per_query,
+                search_category=search_category,
+                number_of_days_back=number_of_days_back,
+                max_results=max_results_per_query,
             )
         )
-
         unique_sources = deduplicate_sources(search_response=search_docs)
-        """
-        source_str = deduplicate_and_format_sources(search_response=search_docs,
-                                                    max_tokens_per_source=self.max_tokens_per_source,
-                                                    include_raw_content=self.include_raw_content)
-        """
-
         return unique_sources
